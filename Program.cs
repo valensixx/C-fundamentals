d8e18db09a782bba7b9ee55610cb93c953e1ea5b
valensixx <valensixx@gmail.com>
2023-04-20 18:28:07 +0300

    changes

--- Program.cs
+++ Program.cs
@@ -11,11 +11,26 @@ for (int i = 0; i < 15; i++)
 int number = 5;//asign int number = 5
 number = 10; //now number takes the value of 10 
 
+float bodyTemp = 36.6f; //add f for float
+double bodyTemp2 = 36.6;
+decimal money = 200.5m; // add m for decimal
 
+bool check = true; //boolean can be only true or false
+bool secondCheck = false;
+
+char symbol = '*';
+
+string name = "Gosho";
 
 // example of wrong names: 
 int myFirstIntVar = 3;
 int myfirstintvar = 8; //the code will work, but it is wrong! 
 
+//calculate 
 
+int a = 5;
+int b = 10;
+int calc = a + b;
+
+Console.WriteLine(calc);
 
