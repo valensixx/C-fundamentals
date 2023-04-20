2679af43ce3ef5e3c9025d98d349a99339d96677
valensixx <valensixx@gmail.com>
2023-04-20 17:14:23 +0300

    "Commit All"

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// Create simple for loop 
+
+for (int i = 0; i < 15; i++)
+{
+    Console.WriteLine(i);
+}
