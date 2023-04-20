b983dca78deb35ee0b50cab2e3dc5dd4e32e5412
valensixx <valensixx@gmail.com>
2023-04-20 18:08:22 +0300

    changes

--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
 
 // Create simple for loop 
-
 for (int i = 0; i < 15; i++)
 {
     Console.WriteLine(i);
@@ -10,11 +8,14 @@ for (int i = 0; i < 15; i++)
 
 
 // data types
-
 int number = 5;//asign int number = 5
 number = 10; //now number takes the value of 10 
 
-// example of wrong names: 
 
+
+// example of wrong names: 
 int myFirstIntVar = 3;
 int myfirstintvar = 8; //the code will work, but it is wrong! 
+
+
+
