f25794331bd7890603e8a469295a7be0ebcab9c8
valensixx <valensixx@gmail.com>
2023-04-20 18:07:43 +0300

    changes

--- Program.cs
+++ Program.cs
@@ -7,3 +7,14 @@ for (int i = 0; i < 15; i++)
 {
     Console.WriteLine(i);
 }
+
+
+// data types
+
+int number = 5;//asign int number = 5
+number = 10; //now number takes the value of 10 
+
+// example of wrong names: 
+
+int myFirstIntVar = 3;
+int myfirstintvar = 8; //the code will work, but it is wrong! 
